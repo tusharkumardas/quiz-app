29ce7fd8634e1376e67787f2fcd2bebc4aa72142
TUSHAR KUMAR DAS <141891214+tusharkumardas@users.noreply.github.com>
2025-05-14 18:55:02 +0530

    Created a quiz interface
    Created a quiz interface with html css and java script for a IEEE quiz event for the students in my university .

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What year was IEEE founded?",
+        answer: "C. 1963",
+        options: [
+            "A. 1875",
+            "B. 1912",
+            "C. 1963",
+            "D. 1987"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does the acronym IEEE stand for?",
+        answer: "B. Institute of Electrical and Electronics Engineers",
+        options: [
+            "A. International Electrotechnical Engineering Establishment",
+            "B. Institute of Electrical and Electronics Engineers",
+            "C. International Energy and Engineering Enterprise",
+            "D. Institution of Engineers in Electronics and Electrics"
+        ]
+    },
+    {
+        numb: 3,
+        question: "IEEE is known for its extensive work in setting standards. Which of these is an IEEE standard for Wi-Fi?",
+        answer: "C. IEEE 802.11",
+        options: [
+            "A. IEEE 1394",
+            "B. IEEE 802.3",
+            "C. IEEE 802.11",
+            "D. IEEE 1609"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which IEEE standard is widely used for Ethernet communication?",
+        answer: "B. IEEE 802.3",
+        options: [
+            "A. IEEE 802.1",
+            "B. IEEE 802.3",
+            "C. IEEE 802.11",
+            "D. IEEE 754"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which IEEE technical field focuses on the development of smart grid technologies?",
+        answer: "B. Power & Energy",
+        options: [
+            "A. Robotics",
+            "B. Power & Energy",
+            "C. Communications",
+            "D. Aerospace"
+        ]
+    }
+]
